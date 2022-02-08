dc5e3f4554d891307d05daa25c1c35a9d9dc2604
Tufahel <tufahel96@gmail.com>
2022-02-09 00:35:58 +0600

    make class and implement displayBook and addBook function

--- script.js
+++ script.js
@@ -1,97 +1,56 @@
-(function () {
-  let lastId = 0;
-  const showList = document.getElementById('show_list');
-  const btnSave = document.getElementById('save');
-  let removeData;
-  let dataList;
-
-  function init() {
-    if (window.localStorage.getItem('dataList')) {
-      dataList = JSON.parse(window.localStorage.getItem('dataList'));
-    } else {
-      dataList = [];
+//Book class: represents a book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
     }
-    btnSave.addEventListener('click', saveData);
-    showData();
-  }
+}
 
-  function showData() {
-    if (dataList.length) {
-      getLastTaskId();
-      for (const item in dataList) {
-        const data = dataList[item];
-        addDataToList(data);
-      }
-      syncEvents();
+
+// ui class: handle ui tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John doe',
+                isbn: '32435'
+            },
+            {
+                title: 'Book Two',
+                author: 'John doe',
+                isbn: '453565'
+            }
+        ];
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
     }
-  }
 
-  function saveData() {
-    const data = {
-      dataId: lastId,
-      dataTitle: document.getElementById('title').value,
-      dataAuthor: document.getElementById('author').value,
-    };
-    dataList.push(data);
-    syncTask();
-    addDataToList(data);
-    syncEvents();
-    lastId++;
-  }
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
 
-  function addDataToList(data) {
-    const removeData = document.createElement('span');
-    const element = document.createElement('li');
-    const line = document.createElement('div');
+        const row = document.createElement('tr');
 
-    element.setAttribute('id', data.dataId);
-    if (data.dataTitle !== '' && data.dataAuthor !== '') {
-      element.innerHTML += `${data.dataTitle}<br>${data.dataAuthor}<br> `;
-
-      removeData.innerHTML = '<button>Remove</button>';
-      removeData.className = 'remove_data';
-      removeData.setAttribute('title', 'Remove');
-
-      line.innerHTML = '<div class="line"></div>';
-
-      element.appendChild(removeData);
-      showList.appendChild(element);
-      element.appendChild(line);
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+        list.appendChild(row);
     }
-  }
+}
 
-  function removeDta(event) {
-    const dataToRemove = event.currentTarget.parentNode;
-    console.log('data to remove', dataToRemove);
-    const dataId = dataToRemove.id;
-    console.log('data to remove id', dataToRemove.id);
-    showList.removeChild(dataToRemove);
-    dataList.forEach((value, i) => {
-      if (value.dataId == dataId) {
-        dataList.splice(i, 1);
-      }
-    });
-    syncTask();
-  }
+//store class: handle storage
 
-  function syncTask() {
-    window.localStorage.setItem('dataList', JSON.stringify(dataList));
-    dataList = JSON.parse(window.localStorage.getItem('dataList'));
-  }
 
-  function getLastTaskId() {
-    const lastTask = dataList[dataList.length - 1];
-    lastId = lastTask.dataId + 1;
-  }
+//event: display books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-  function syncEvents() {
-    removeData = document.getElementsByClassName('remove_data');
-    if (removeData.length) {
-      for (let i = 0; i < removeData.length; i++) {
-        removeData[i].addEventListener('click', removeDta);
-      }
-    }
-  }
 
-  init();
-}());
+//event: add a book
+
+
+//event: remove a book
